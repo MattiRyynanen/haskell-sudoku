71fe6d31002ef85cccc5c74f528da79a1063cab7
Matti Ryynänen <matti.ryynanen@unity3d.com>
2021-06-15 20:50:25 +0300

    Formulating recursion.

--- recordsudoku.hs
+++ recordsudoku.hs
@@ -1,13 +1,7 @@
-import qualified Data.Set as Set
-
---Set.fromList [1..9]
-
 type Index = Int
 type Candidate = Int
 data Cell = Cell { index :: Index, candidates :: [Candidate] } deriving Show
 type Puzzle = [Cell]
-
-a = Cell { index = 3, candidates=[1,2] }
 
 blockOf :: Cell -> Index
 blockOf cell = 3 * (rowOf cell `div` 3) + colOf cell `div` 3
@@ -21,11 +15,14 @@ colOf cell = index cell `rem` 9
 posOf :: Cell -> String
 posOf c = concat $ map (\f -> show $ f c) [rowOf, colOf, blockOf]
 
-intersects :: Cell -> Cell -> Bool
-intersects a b = notSame && anyIntersection
+numCandidates c = length $ candidates c
+isSolved c = numCandidates c == 1
+
+intersectsEx :: Cell -> Cell -> Bool
+intersectsEx a b = notSame && anyIntersection
     where ops = [rowOf, colOf, blockOf]
           notSame = index a /= index b
-          anyIntersection = or [i == j | (i, j) <- zip (map ($ a) ops) (map ($ b) ops)]
+          anyIntersection = or $ zipWith (==) (map ($ a) ops) (map ($ b) ops)
 
 hasCand :: Cell -> Candidate -> Bool
 hasCand cell cand = elem cand (candidates cell)
@@ -42,20 +39,25 @@ applyAt :: Index -> (Cell -> Cell) -> Puzzle -> Puzzle
 applyAt i f puzzle = applyWhen (\c -> index c == i) f puzzle
 
 withNo c = filter (/=c)
-cellAt puzzle i = head $ filter (\c -> index c == i) puzzle
-removeCellCandidate cell cand = cell { candidates = withNo cand (candidates cell) }
-setCellCandidate cell cand = cell { candidates = [cand] }
+removeCellCandidate cand cell = cell { candidates = withNo cand (candidates cell) }
+setCellCandidate cand cell = cell { candidates = [cand] }
 
-removeCandidate :: Puzzle -> Cell -> Candidate -> Puzzle
-removeCandidate puzzle cell cand = newPuzzle
-    where newPuzzle = applyWhen sameCell removeCandidate puzzle
-          sameCell c = index c == index cell
-          removeCandidate c = removeCellCandidate c cand
+samePosThan a b = index a == index b
+
+removeCandidate :: Puzzle -> Candidate -> Index -> Puzzle
+removeCandidate puzzle cand ind
+    | isSolved cell || hasNoCand cell cand = puzzle
+    | numCandidates cell == 2 = setFinal puzzle cell cand
+    | otherwise = applyWhen (samePosThan cell) (removeCellCandidate cand) puzzle
+    where cell = puzzle !! ind
 
 setFinal :: Puzzle -> Cell -> Candidate -> Puzzle
-setFinal puzzle cell cand = newPuzzle
-    where newPuzzle = applyWhen sameCell setCandidate puzzle
-          sameCell c = index c == index cell
-          setCandidate c = setCellCandidate c cand
+setFinal puzzle cell final
+    | isSolved cell = error "Can't set final for a solved cell."
+    | hasNoCand cell final = error "Can't set final since it is not in cell candidates."
+    | otherwise = broadcastFinal withFinal
+    where withFinal = applyWhen (samePosThan cell) (setCellCandidate final) puzzle
+          intersectIndx = map (index) $ filter (intersectsEx cell) withFinal
+          broadcastFinal puz = foldl (\p -> removeCandidate p final) puz intersectIndx
 
 p = [Cell {index = i, candidates=[1..9]} | i <- [0..80]]
