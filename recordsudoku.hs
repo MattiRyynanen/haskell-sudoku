433fefccdb355f1c78ca2930532695a8ef492772
Matti Ryynänen <matti.ryynanen@unity3d.com>
2021-06-16 20:53:53 +0300

    Added some printing + fixed applying final value with removeCandidate

--- recordsudoku.hs
+++ recordsudoku.hs
@@ -1,3 +1,4 @@
+import Data.List (intercalate)
 import qualified Data.Char
 import qualified SamplePuzzles
 
@@ -50,9 +51,10 @@ samePosThan a b = index a == index b
 removeCandidate :: Puzzle -> Candidate -> Index -> Puzzle
 removeCandidate puzzle cand ind
     | isSolved cell || hasNoCand cell cand = puzzle
-    | numCandidates cell == 2 = setFinal puzzle cell cand
+    | numCandidates cell == 2 = setFinal puzzle cell final_value
     | otherwise = applyWhen (samePosThan cell) (removeCellCandidate cand) puzzle
     where cell = puzzle !! ind
+          final_value = head $ withNo cand $ candidates cell
 
 setFinal :: Puzzle -> Cell -> Candidate -> Puzzle
 setFinal puzzle cell final
@@ -71,4 +73,33 @@ loadPuzzle puzzle_str = foldl applyValue createEmptyPuzzle indx_values
     where indx_values = [(i, Data.Char.digitToInt(s)) | (i, s) <- zip [0..] puzzle_str, s /= '.']
           applyValue p iv = setFinal p (p !! fst iv) (snd iv)
 
-p = loadPuzzle SamplePuzzles.xtr_sud_04
+pl = loadPuzzle SamplePuzzles.xtr_sud_04
+
+withColor :: Show a => a -> [Char] -> [Char]
+withColor c str = concat ["\ESC[", show c, "m", str, "\ESC[0m"]
+
+showCell :: Cell -> String
+showCell c
+    | numCandidates c == 1 = withColor 32 (cellToStr " " c)
+    | numCandidates c == 2 = withColor 33 (cellToStr "." c)
+    | otherwise = cellToStr "." c
+    where cellToStr sep c = concat $ [if hasCand c cand then show cand else sep | cand <- [1..9]]
+
+group :: Int -> [a] -> [[a]]
+group _ [] = []
+group c xs = take c xs : group c (drop c xs)
+
+noHighlights :: [Bool]
+noHighlights = replicate (9 * 9) False
+
+showPuzzle :: Puzzle -> String
+showPuzzle cells = showPuzzleHighlights cells noHighlights
+
+showPuzzleHighlights :: Puzzle -> [Bool] -> String
+showPuzzleHighlights cells highlights = intercalate line (map concat (group 3 rows))
+    where cellContents = [if h then withColor 44 (showCell c) else showCell c | (c, h) <- zip cells highlights]
+          rows = map ('\n':) $ map (intercalate "|") $ group 3 $ map (intercalate " ") $ group 3 cellContents
+          line = '\n' : (intercalate "+" $ replicate 3 (replicate 29 '-'))
+
+printPuzzle :: Puzzle -> IO ()
+printPuzzle cells = putStrLn $ showPuzzle cells
