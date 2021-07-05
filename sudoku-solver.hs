70c9d5f5483ec3bdbb957b08c9728d2c0d0500ff
Matti Ryynänen <matti.ryynanen@unity3d.com>
2021-07-05 10:32:16 +0300

    Revisiting main function.

--- sudoku-solver.hs
+++ sudoku-solver.hs
@@ -16,7 +16,11 @@ getSolution line
 eachLine :: (String -> String) -> (String -> String)
 eachLine f = unlines . map f . lines
 
-main = interact (eachLine getSolution)
+main = do
+    putStrLn "Sudoku solver. Input a sudoku with a line and press enter"
+    putStrLn ".8....3.1 .143..... ..5.7...9 5..6..1.. 42..5..87 ..8..7..5 2...8.5.. .....674. 8.6....1."
+    interact (eachLine getSolution)
+    putStrLn "Solver session finished."
 
 -- Building profile
 -- stack ghc -- -prof -fprof-auto -rtsopts sudoku-solver.hs
