d3b56bd63369f07a27df52c7728d436cd933135a
Matti Ryynänen <matti.ryynanen@unity3d.com>
2021-07-06 16:07:31 +0300

    Added some more challenging puzzles.

--- dev-bench.hs
+++ dev-bench.hs
@@ -4,6 +4,6 @@ import Solvers
 import SamplePuzzles
 import Data.Maybe
 
-pl = loadPuzzle SamplePuzzles.level5_hs_2020_07_05
+pl = loadPuzzle "..68.5..7 2.......3 ..5..6.2. .....9.6. ..4...5.. .7.5..... .9.4..1.. 1.......4 4..9.78.."--SamplePuzzles.level5_hs_2020_07_05
 solutions = beginSolve (fromJust pl)
 px = getPuzzle $ head solutions
