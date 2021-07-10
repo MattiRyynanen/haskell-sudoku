c43bf8dd14b386173c9f15005c7b3fe3078e121a
Matti Ryynänen <matti.ryynanen@unity3d.com>
2021-07-10 15:10:47 +0300

    Adding some simple tests.

--- test/SnippetsSpec.hs
+++ test/SnippetsSpec.hs
@@ -2,16 +2,26 @@ module SnippetsSpec (spec) where
 
 import Snippets
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+import Data.List (nub)
+
+prop_hasOne :: [Int] -> Bool
+prop_hasOne xs = hasOne xs == (==1) (length xs)
+
+prop_allSame :: [Int] -> Property
+prop_allSame xs = not (null xs) ==> allSame xs == (==1) (length (nub xs))
 
 spec :: Spec
 spec = do
     describe "Snippets.hasOne" $ do
+        prop "is equal to length being one" prop_hasOne
+        it "returns True if list has one element, otherwise False" $ do
+            map hasOne ["", "a", "ab"] `shouldBe` [False, True, False]
 
-        it "returns True for lists with one element" $ do
-            hasOne "a" `shouldBe` True
+    describe "Snippets.allSame ([a])" $ do
+        it "returns True if all list elements are equal" $ do
+            allSame ["a", "a", "a"] `shouldBe` True
 
-        it "returns False for empty lists" $ do
-            hasOne "" `shouldBe` False
-
-        it "returns False for lists longer than one" $ do
-            hasOne "ab" `shouldBe` False
+        prop "is same as having only one unique element" prop_allSame
